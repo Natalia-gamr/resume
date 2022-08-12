74b160bfe2eaa700ecc35ae87d80bea61ca2b28f
Natalie <fogel666@yandex.ru>
2022-08-12 18:12:15 +0300

    add all

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.main__gallery_items').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    
+ });
+      
